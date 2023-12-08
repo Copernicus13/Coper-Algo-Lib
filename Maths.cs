198a830f9497f4098c36768eecb4574a537a7c87
Michel P. <copernicus_13@hotmail.com>
2023-12-08 23:07:50 +0100

    [Maths] Add GCD and LCM methods.

--- Maths.cs
+++ Maths.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace CoperAlgoLib
 {
     public static class Maths
     {
-        public static IEnumerable<int> GetFactors(int x)
+        public static IEnumerable<int> GetAllDivisors(int x)
         {
             for (int factor = 1; factor * factor <= x; ++factor)
                 if (x % factor == 0)
@@ -13,6 +14,22 @@ namespace CoperAlgoLib
                     if (factor * factor != x)
                         yield return x / factor;
                 }
+        }
+
+        /// <remarks>
+        /// With <see cref="BigInteger"/>, use directly <code>BigInteger.GreatestCommonDivisor()</code>
+        /// </remarks>
+        public static T GCD<T>(T a, T b) where T : IBinaryInteger<T>
+        {
+            while (!T.IsZero(b))
+                (a, b) = (b, a % b);
+            return a;
+        }
+
+        public static T LCM<T>(T a, T b) where T : IBinaryInteger<T>
+        {
+            T gcd = GCD(a, b);
+            return T.IsZero(gcd) ? T.Zero : a / gcd * b;
         }
     }
 }

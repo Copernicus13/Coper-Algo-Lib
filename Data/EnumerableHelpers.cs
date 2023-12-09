87aed37ff959dc4889c4e200320c199ecb8047ea
Michel P. <copernicus_13@hotmail.com>
2023-12-09 09:17:02 +0100

    [Data] Add a slightly optimized Reverse() for IEnumerable<T>

---
+++ Data/EnumerableHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoperAlgoLib.Data
+{
+    public static class EnumerableHelpers
+    {
+        public static IEnumerable<T> ReverseEx<T>(this IEnumerable<T> coll)
+        {
+            // if coll is indexable
+            if (coll is IList<T> quick)
+                for (int i = quick.Count - 1; i >= 0; --i)
+                    yield return quick[i];
+            else
+                foreach (var item in coll.Reverse())
+                    yield return item;
+        }
+    }
+}
